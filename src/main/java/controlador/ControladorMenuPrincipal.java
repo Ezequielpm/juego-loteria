f7b6fef5590fed49ed7b4febfdaa106a6672c6c9
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-10-23 10:14:43 -0600

    Se agrega un efecto de hover a los botones (se cambia el color del boton al pasar el mouse encima)

--- src/main/java/controlador/ControladorMenuPrincipal.java
+++ src/main/java/controlador/ControladorMenuPrincipal.java
@@ -4,6 +4,7 @@
  */
 package controlador;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -14,7 +15,7 @@ import vista.MenuPrincipal;
  *
  * @author ezequielpena
  */
-public class ControladorMenuPrincipal implements ActionListener {
+public class ControladorMenuPrincipal implements ActionListener,MouseListener {
     MenuPrincipal objMenuPrincipal;
     
     public ControladorMenuPrincipal(){
@@ -28,6 +29,12 @@ public class ControladorMenuPrincipal implements ActionListener {
         this.objMenuPrincipal.botonSettings.addActionListener(this);
         this.objMenuPrincipal.botonAbout.addActionListener(this);
         this.objMenuPrincipal.botonExit.addActionListener(this);
+        
+        this.objMenuPrincipal.botonPlay.addMouseListener(this);
+        this.objMenuPrincipal.botonProfile.addMouseListener(this);
+        this.objMenuPrincipal.botonSettings.addMouseListener(this);
+        this.objMenuPrincipal.botonAbout.addMouseListener(this);
+        this.objMenuPrincipal.botonExit.addMouseListener(this);
     }
 
     @Override
@@ -50,6 +57,69 @@ public class ControladorMenuPrincipal implements ActionListener {
         }
         if(e.getSource()==this.objMenuPrincipal.botonExit){
             //Boton exit
+            return;
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if(e.getSource()==this.objMenuPrincipal.botonPlay){
+            this.objMenuPrincipal.botonPlay.setBackground(Color.GREEN);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonProfile){
+            this.objMenuPrincipal.botonProfile.setBackground(Color.GREEN);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonSettings){
+            this.objMenuPrincipal.botonSettings.setBackground(Color.GREEN);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonAbout){
+            this.objMenuPrincipal.botonAbout.setBackground(Color.GREEN);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonExit){
+            this.objMenuPrincipal.botonExit.setBackground(Color.GREEN);
+            return;
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if(e.getSource()==this.objMenuPrincipal.botonPlay){
+            this.objMenuPrincipal.botonPlay.setBackground(Color.YELLOW);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonProfile){
+            this.objMenuPrincipal.botonProfile.setBackground(Color.YELLOW);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonSettings){
+            this.objMenuPrincipal.botonSettings.setBackground(Color.YELLOW);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonAbout){
+            this.objMenuPrincipal.botonAbout.setBackground(Color.YELLOW);
+            return;
+        }
+        if(e.getSource()==this.objMenuPrincipal.botonExit){
+            this.objMenuPrincipal.botonExit.setBackground(Color.YELLOW);
             return;
         }
     }
