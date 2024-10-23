5ae02422b78f4328faffe0d2b54f6098f007dd75
Ezequiel <ezequielmazariegossolis@gmail.com>
2024-10-23 09:21:43 -0600

    Inicio del repositorio del proyecto de loteria

---
+++ src/main/java/controlador/ControladorMenuPrincipal.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import vista.MenuPrincipal;
+
+/**
+ *
+ * @author ezequielpena
+ */
+public class ControladorMenuPrincipal {
+    MenuPrincipal objMenuPrincipal;
+    
+    public ControladorMenuPrincipal(){
+        
+    }
+    
+    public ControladorMenuPrincipal(MenuPrincipal objMenuPrincipal){
+        this.objMenuPrincipal = objMenuPrincipal;
+        
+    }
+
+    
+}
